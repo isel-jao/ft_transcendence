4e5b1a55ca80298195e24ea727f590af4154af04
isel-jao <iseljao89@gmail.com>
2022-09-30 14:40:27 +0100

    initial commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>home</title>
+        <meta name="ft_transcendence" content="ft_transcendence" />
+      </Head>
+      <main className="bg-cyan-500">app</main>
+    </div>
+  );
+};
+
+export default Home;
