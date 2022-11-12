20839253e0ea9a59c7544aa636e1944b30974994
Noreddine Hakkaoui <nhakkaou@e2r4p9.1337.ma>
2022-11-12 17:57:18 +0100

    Home done

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,13 +1,73 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import Image from "next/image";
+import styled from "styled-components";
+import { Button } from "../../components/style/Home";
+import Card from "../../components/cardGame";
+
+const Div = styled.div`
+  display: flex;
+  padding: 80px;
+  width: 100%;
+  height: 50%;
+  color: #ffff;
+  align-items: center;
+  .title {
+    width: 100%;
+    span {
+      font-weight: bold;
+      font-size: 3.2rem;
+    }
+    p {
+      padding-top: 20px;
+      padding-bottom: 20px;
+    }
+  }
+  .player {
+    width: 100%;
+  }
+`;
+const Main = styled.div`
+  width: 100%;
+  color: #fff;
+  > div {
+    text-align: center;
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+  > .Cards {
+    gap: 40px;
+    flex-wrap: wrap;
+    display: flex;
+    grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  }
+`;
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>home</title>
-        <meta name="ft_transcendence" content="ft_transcendence" />
-      </Head>
-      <main>home</main>
+      <Div>
+        <div className="title">
+          <span>Win a Game</span>
+          <p>Let’s start a game against your friend or a random people.</p>
+          <Button type="submit" value="PLAY NOW" />
+        </div>
+        <div className="player">
+          <Image src="/icons/Player.svg" width={500} height={500} />
+        </div>
+      </Div>
+      <Main>
+        <div style={{ marginBottom: "20px" }}>Recent games</div>
+        <div className="Cards">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </Main>
     </div>
   );
 };
